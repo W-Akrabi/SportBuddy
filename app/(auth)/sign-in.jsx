79bcbe7d8d670c8ef6330006d9cadadc3e42a898
WaleedAkrabi <waleed.akrabi@mail.utoronto.ca>
2024-05-31 15:18:24 +0400

    log in screen

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -1,11 +1,44 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, ScrollView, Image} from 'react-native'
+import {React, useState} from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import {images} from '../../constants'
+import FormFeild from '../../components/FormFeild'
 
 const SignIn = () => {
+  const [form, setForm] = useState({
+    email: '',
+    password: ''
+  })
   return (
-    <View>
-      <Text>SignIn</Text>
-    </View>
+    <SafeAreaView className="bg-black h-full">
+      <ScrollView>
+        <View className="w-full justify-center h-full px-4 my-6">
+          <Image source={images.logo}
+          resizeMode='contain' className="w-[125px] h-[35px]"/>
+
+          <Text className="text-2xl text-white text-semibold mt-2 font-psemibold">
+            Log in to Sporti
+          </Text>
+
+          <FormFeild
+            title="Email"
+            value={form.email}
+            handleChangeText={(e) => setForm({ ...form,
+            email: e })}
+            otherStyles="mt-7"
+            keyboardType="email-address"
+          />
+
+          <FormFeild
+            title="Password"
+            value={form.password}
+            handleChangeText={(e) => setForm({ ...form,
+            password: e })}
+            otherStyles="mt-7"
+          />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   )
 }
 
