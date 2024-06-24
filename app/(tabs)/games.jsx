e6793e165aa67631fe19d6d1ea64caf38743cbd5
WaleedAkrabi <waleed.akrabi@mail.utoronto.ca>
2024-06-24 23:02:36 +0400

    game page

--- app/(tabs)/games.jsx
+++ app/(tabs)/games.jsx
@@ -1,11 +1,31 @@
-import { View, Text } from 'react-native'
+import { View, Text, SafeAreaView, FlatList } from 'react-native'
 import React from 'react'
 
 const Games = () => {
   return (
-    <View>
-      <Text>Games</Text>
-    </View>
+    <SafeAreaView className="bg-black">
+      <FlatList
+        data={[{ id: 1}, {id: 2}, {id: 3}]}
+        keyExtractor={(item) => item.$id}
+        renderItem={({item}) => (
+          <Text className="text-3xl">{item.id}</Text>
+        )}
+        ListHeaderComponent={() => (
+          <View className="my-6 px-4 space-y-6">
+            <View className="justify-between items-start flex-row mb-6">
+              <View>
+                <Text className="font-pmedium text-sm text-gray-100">
+                  Welcome Back
+                </Text>
+                <Text>
+                  Waleed
+                </Text>
+              </View> 
+            </View>
+          </View>
+        )}
+      />
+    </SafeAreaView>
   )
 }
 
